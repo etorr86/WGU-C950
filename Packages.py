bfa14c4ee5964fa9c0e32e6746f899653598e39c
etorr86 <etorr86@wgu.edu>
2020-09-15 21:21:56 -0400

    modified package class and use it in csv file

---
+++ Packages.py
@@ -0,0 +1,51 @@
+class Packages:
+    def __init__(self,
+                 package_key=None,
+                 package_address=None,
+                 package_city=None,
+                 package_state=None,
+                 package_zip_code=None,
+                 package_delivery_time=None,
+                 package_weight=None,
+                 package_notes=None):
+        self._packageKey = package_key
+        self._packageAddress = package_address
+        self._packageCity = package_city
+        self._packageState = package_state
+        self._packageZipCode = package_zip_code
+        self._packageDeliveryTime = package_delivery_time
+        self._packageWeight = package_weight
+        self._packageNotes = package_notes
+
+    def package_key(self):
+        return self._packageKey
+
+    def package_address(self):
+        return self._packageAddress
+
+    def package_city(self):
+        return self._packageCity
+
+    def package_state(self):
+        return self._packageState
+
+    def package_zip_code(self):
+        return self._packageZipCode
+
+    def package_delivery_time(self):
+        return self._packageDeliveryTime
+
+    def package_weight(self):
+        return self._packageWeight
+
+    def package_notes(self):
+        return self._packageNotes
+
+    def full_address(self):
+        return self._packageAddress + " " + self._packageCity + " " + self._packageState
+
+    def __repr__(self):
+        template = '{0._packageKey}, {0._packageAddress}, {0._packageCity}, ' \
+                   '{0._packageState}, {0._packageZipCode}, {0._packageDeliveryTime}, ' \
+                   '{0._packageWeight}, {0._packageNotes}'
+        return template.format(self)
--- ReadCSV.py
+++ ReadCSV.py
@@ -2,15 +2,28 @@ import csv
 
 # Open CSV File
 from HashTable import HashTable
-
+from Packages import Packages
 
 with open('Data/PackageFile.csv') as packageFile:
     readCSV = csv.reader(packageFile, delimiter=',')
     packageHashMap = HashTable()
+    packageMap = HashTable()
+    deliveryPackage = Packages()
 
     # Read values from CSV file and added to the custom hash table, where the key
     # will be the package ID.
     for row in readCSV:
+        # Set the package object (new way)
+        deliveryPackage._packageKey = row[0]
+        deliveryPackage._packageAddress = row[1]
+        deliveryPackage._packageCity = row[2]
+        deliveryPackage._packageState = row[3]
+        deliveryPackage._packageZipCode = row[4]
+        deliveryPackage._packageDeliveryTime = row[5]
+        deliveryPackage._packageWeight = row[6]
+        deliveryPackage._packageNotes = row[7]
+
+        # Set the package object (old way)
         packageKey = row[0]
         packageAddress = row[1]
         packageCity = row[2]
@@ -20,7 +33,9 @@ with open('Data/PackageFile.csv') as packageFile:
         packageWeight = row[6]
         packageNotes = row[7]
 
-        packageValues = [packageAddress,
+        packageList = [deliveryPackage]
+        packageValues = [packageKey,
+                         packageAddress,
                          packageCity,
                          packageState,
                          packageZipCode,
@@ -29,4 +44,8 @@ with open('Data/PackageFile.csv') as packageFile:
                          packageNotes]
 
         packageHashMap.insert(packageKey, packageValues)
+        packageMap.insert(deliveryPackage.package_key(), packageList)
+
+print("Using variables: ", packageHashMap.find('2'))
+print("Using own custom package class: ", packageMap.find('2'))
 
