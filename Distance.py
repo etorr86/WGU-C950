86cf23a9e97799f01fdfb7ac12104a7a9530fc95
etorr86 <etorr86@wgu.edu>
2020-09-21 16:48:38 -0400

    clean the csv loader
    added new csv files, Distance.py and clean the csv loader

---
+++ Distance.py
@@ -0,0 +1,5 @@
+class Distance:
+    def __init__(self, address_to=None, address_from=None, distance=None):
+        self.addressTo = address_to,
+        self.addressFrom = address_from,
+        self.totalDistance = distance
--- ReadCSV.py
+++ ReadCSV.py
@@ -1,17 +1,16 @@
 import csv
+import pandas as pd
 
 # Open CSV File
 from HashTable import HashTable
 from Packages import Packages
 
 with open('Data/PackageFile.csv') as packageFile:
-    readCSV = csv.reader(packageFile, delimiter=',')
-    packageHashMap = HashTable()
+    readPackageCSV = csv.reader(packageFile, delimiter=',')
     packageMap = HashTable()
 
     # Read values from CSV file, insert the values into the package model
-    # insert the package object as a list into the custom hast table.
-    for row in readCSV:
+    # insert the package object into the custom hast table.
+    for row in readPackageCSV:
         deliveryPackage = Packages(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
-        packageMap.insert(deliveryPackage.package_key(), [deliveryPackage])
-
+        packageMap.insert(deliveryPackage.package_key(), deliveryPackage)
